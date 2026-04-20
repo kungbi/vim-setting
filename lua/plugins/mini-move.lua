69a1f0cc033989d892822281d733e28143358cea
woongbi shin <shin.woongbi@linkareer.com>
2026-04-20 13:47:42 +0900

    Add mini.move plugin and prisma prettier formatter
    - Add mini.move with Alt+hjkl mappings for moving lines/selections
    - Add conform.nvim prettier formatter for prisma files

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,16 @@
+return {
+  "echasnovski/mini.move",
+  version = "*",
+  opts = {
+    mappings = {
+      left = "<M-h>",
+      right = "<M-l>",
+      down = "<M-j>",
+      up = "<M-k>",
+      line_left = "<M-h>",
+      line_right = "<M-l>",
+      line_down = "<M-j>",
+      line_up = "<M-k>",
+    },
+  },
+}
--- lua/plugins/prisma.lua
+++ lua/plugins/prisma.lua
@@ -7,4 +7,12 @@ return {
       })
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        prisma = { "prettier" },
+      },
+    },
+  },
 }
