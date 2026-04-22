01672d899415a990615ea379c6a14b359c970c70
woongbi shin <shin.woongbi@linkareer.com>
2026-04-22 10:04:33 +0900

    Switch from neo-tree to snacks explorer, fix mini.move repo name

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      explorer = { enabled = false },
+      explorer = { enabled = true, replace_netrw = false },
       picker = {
         layout = {
           layout = {
