27a25b2f7719d7f0526be87d483bb30eec825acb
woongbi shin <shin.woongbi@linkareer.com>
2026-04-24 12:18:49 +0900

    Add blink.cmp keymap customization (Tab, Enter, C-j/k)

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+    },
+  },
+}
