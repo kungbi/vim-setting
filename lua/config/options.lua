fbeb9d0efce76b82114357aae97a7865acabfac1
kungbi <kipper12335@gmail.com>
2026-05-05 09:12:55 +0900

    fix: clear tmux history on VimEnter to prevent version query leak

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,15 @@
 vim.opt.ttimeout = true
 vim.opt.ttimeoutlen = 10
 
+-- tmux 히스토리 클리어 (버전 쿼리 응답이 입력으로 새어드는 현상 방지)
+if vim.env.TMUX then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      vim.fn.system("tmux clear-history")
+    end,
+  })
+end
+
 -- tmux 안에서 OSC 52 대신 pbcopy/pbpaste 직접 사용
 if vim.env.TMUX then
   vim.g.clipboard = {
