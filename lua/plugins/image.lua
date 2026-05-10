3140e691d6e6ff2fa41cdd9f7cd76f8bf43fc67f
kungbi <kipper12335@gmail.com>
2026-05-11 01:22:22 +0900

    feat: re-enable image.nvim and add SVG hijack support
    Update plugin lockfile with latest commits.

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,7 +1,6 @@
 return {
   {
     "3rd/image.nvim",
-    enabled = false,
     build = "luarocks --local --lua-version=5.1 install magick",
     opts = {
       backend = "kitty",
@@ -26,7 +25,7 @@ return {
       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
       editor_only_render_when_focused = false,
       tmux_show_only_in_active_window = false,
-      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif", "*.svg" },
     },
   },
 }
