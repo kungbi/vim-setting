0c0fa349fe51df4b1de1d66cb7f73ac1cf5b8945
woongbi shin <shin.woongbi@linkareer.com>
2026-05-19 09:53:07 +0900

    feat: add Alt-j/k line move keymaps in normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<M-j>", ":m+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<M-k>", ":m-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 vim.keymap.set("n", "<leader>fy", function() vim.fn.setreg("+", vim.fn.expand("%:t")) end, { desc = "Copy filename" })
